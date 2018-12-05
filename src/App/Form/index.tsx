ea83e73105f5894c101f0db05bd01dae2b80001d
Kishan Devani <kdevani964@gmail.com>
2018-12-05 08:07:41 +0530

    Update in Form.

--- src/App/Form/index.tsx
+++ src/App/Form/index.tsx
@@ -7,7 +7,16 @@ export class Form extends React.Component<any, any> {
     }
 
     render() {
-        return (<div className={'Form'}>
-        </div>);
+        return (
+            <div className={'Form'}>
+                <div className={'Item Header'}>
+                    <div className={'UserImg'}></div>
+                    <div className={'Message'}>
+                        Ask friends to recommend.<br/>
+                        Free. With no middleman.
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
