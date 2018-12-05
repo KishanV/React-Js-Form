4ecf52d812f66992166d99c0629ff69b74f486bb
Kishan Devani <kdevani964@gmail.com>
2018-12-05 07:31:16 +0530

    Introduced Form Component.

--- src/App/Form/index.tsx
+++ src/App/Form/index.tsx
@@ -1,15 +1,13 @@
 import React = require("react");
 import './index.scss';
-import {Form} from "./Form";
 
-export class Index extends React.Component<any, any> {
+export class Form extends React.Component<any, any> {
     constructor(props: any) {
         super(props);
     }
 
     render() {
-        return (<div className={'App'}>
-            <Form/>
+        return (<div className={'Form'}>
         </div>);
     }
 }
