d2ecdd8641b4d5b9c6905ec1967719d6dc1197c4
Kishan Devani <kdevani964@gmail.com>
2018-12-05 07:08:04 +0530

    Introduced ReactJs Container and App view.

---
+++ src/App/index.tsx
@@ -0,0 +1,13 @@
+import React = require("react");
+
+export class Index extends React.Component<any, any> {
+    constructor(props: any) {
+        super(props);
+    }
+
+    render() {
+        return (<div className={'Dashboard'}>
+
+        </div>);
+    }
+}
---
+++ src/index.tsx
@@ -0,0 +1,37 @@
+import './index.scss';
+import {Index} from "./App";
+import ReactDOM = require("react-dom");
+import React = require("react");
+import {Provider} from 'react-redux';
+import {combineReducers, createStore} from "redux";
+
+const reducer = combineReducers({
+    selectedLevel:(state = 1, action:any) => {
+        return (action.selectedLevel === undefined ? state : action.selectedLevel);
+    },
+    selectedStage:(state = 1, action:any) => {
+        return (action.selectedStage === undefined ? state : action.selectedStage);
+    },
+    selectedSquare:(state = 1, action:any) => {
+        return (action.selectedSquare === undefined ? state : action.selectedSquare);
+    }
+});
+
+const store = createStore(reducer);
+
+window.oncontextmenu = () => {
+    return false;
+};
+
+const appElm = document.createElement('div');
+appElm.classList.add('Container');
+document.body.appendChild(appElm);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Index/>
+    </Provider>,
+    appElm
+);
+
+
