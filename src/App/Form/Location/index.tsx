e1c33988cf198a9531147ce609ac375c4dad2aae
Kishan Devani <kdevani964@gmail.com>
2018-12-06 06:33:04 +0530

    Added toggle button.

---
+++ src/App/Form/Location/index.tsx
@@ -0,0 +1,40 @@
+import React = require("react");
+import './index.scss';
+export class Location extends React.Component<any, any> {
+    state : {
+        anyLocation:boolean
+    } = {
+        anyLocation:true
+    };
+
+    constructor(props: any) {
+        super(props);
+        this.onClick = this.onClick.bind(this);
+    }
+
+    onClick() {
+        if(this.state.anyLocation){
+            this.setState({
+                anyLocation:false
+            })
+        } else {
+            this.setState({
+                anyLocation:true
+            })
+        }
+    }
+
+    render() {
+        return (
+            <div className={'Box'}>
+                <input placeholder={'Location'}/>
+                <div onClick={this.onClick} className={'Toggle ' + (this.state.anyLocation ? 'True' : '')}>
+                    <div className={'Lable'}>{'Any location'}</div>
+                    <div className={'Control'}>
+                        <div className={'Button'}></div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
