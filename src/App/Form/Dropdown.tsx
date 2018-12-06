0292cd51612edf9753e612d676e115516cf47406
Kishan Devani <kdevani964@gmail.com>
2018-12-06 07:10:03 +0530

    Dropdown update.

--- src/App/Form/Dropdown.tsx
+++ src/App/Form/Dropdown.tsx
@@ -7,11 +7,15 @@ export class Dropdown extends React.Component<any, any> {
         this.onKeyUp = this.onKeyUp.bind(this);
     }
 
-    state:{
-        value:string
+    state: {
+        value: string,
+        sugetions: string[],
+        movement: boolean
     } = {
-        value:''
-    }
+        value: '',
+        sugetions: [],
+        movement: false
+    };
 
     getList(): React.ReactNode[] {
         const strList = [
@@ -24,31 +28,53 @@ export class Dropdown extends React.Component<any, any> {
         ];
         const elmList: React.ReactNode[] = [];
         const value = this.state.value.toLocaleLowerCase().trim();
-        if(this.state.value.trim().length != 0){
+        if (this.state.movement === true) {
             for (let index = 0; index < strList.length; index++) {
                 const currentString = strList[index].toLocaleLowerCase().trim();
-                console.log(value,currentString);
-                if(currentString.startsWith(value)){
+                console.log(value, currentString);
+                if (currentString.startsWith(value) || value.length === 0) {
                     elmList.push(
                         <div key={index} className={'Item'}>{strList[index]}</div>
                     );
                 }
             }
         }
-
         return elmList;
     }
 
-    onKeyUp(e:React.KeyboardEvent) {
+    componentDidMount() {
+        let isDown = false;
+        const self = this;
+        document.addEventListener('mouseup', () => {
+            if (!isDown) {
+                self.setState({
+                    movement: false
+                });
+            }
+        });
+        document.addEventListener('mousedown', () => {
+            isDown = false;
+        });
+        if (this.refs.rootElm) {
+            const rootElm: HTMLElement = this.refs.rootElm as HTMLElement;
+            rootElm.addEventListener('mousedown', () => {
+                isDown = true;
+            });
+        }
+    }
+
+    onKeyUp(e: React.KeyboardEvent) {
         const input = e.target as HTMLInputElement;
         this.setState({
-            value:input.value
-        })
+            value: input.value,
+            movement: true
+        });
+        console.log(this.refs);
     }
 
     render() {
         return (
-            <div className={'Box Dropdown'}>
+            <div ref={'rootElm'} className={'Box Dropdown'}>
                 <input onKeyUp={this.onKeyUp} placeholder={"I'am looking for.."}/>
                 <div className={'List'}>
                     {this.getList()}
